35b15b9e766df37e2e0f1da255d75a38355402e8
HAFSA <bouzidihafsa75@gmail.com>
2026-09-14 23:03:03 +0100

    Update API URL to Railway

--- src/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://projetgestionmarchesbackend-production.up.railway.app' // Votre URL Railway
-};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://projetgestionmarchesbackend-production.up.railway.app'
+};
